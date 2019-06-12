06ce611578f4fbd8903c8032767cfffec3921eef
Alex Felipe <alexfelipevieira@gmail.com>
2019-06-12 10:09:17 -0300

    4.1

--- app/src/main/java/br/com/alura/aluraesporte/ui/fragment/PagamentoFragment.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/PagamentoFragment.kt
@@ -86,7 +86,7 @@ class PagamentoFragment : Fragment() {
     }
 
     private fun vaiParaListaProdutos() {
-        controlador.popBackStack(R.id.acao_pagamento_para_listaProduto, false)
+        controlador.navigate(R.id.acao_pagamento_para_listaProduto)
     }
 
     private fun criaPagamento(): Pagamento? {
